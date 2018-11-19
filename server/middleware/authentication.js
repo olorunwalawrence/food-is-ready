6c32debeaee15a0ecfed699f405883c3916ae257
olorunwalawrence <olorunwalawrence5@gmail.com>
2018-11-19 17:18:50 +0100

    chore(setup): configure jwt
    - install jsonwebtoken
    - setup jsonwebtoken
    #161532935

---
+++ server/middleware/authentication.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import {config} from 'dotenv';
+
+consfig();
+
+const secret = process.env.SECRET;
+
+const verifyUser = (req, res, next) => {
+	const token = req.headers['x-access-token'];
+	if (!token) {
+		res.status(301).json({
+			success: false,
+			message: 'authentication failed, please login'
+		});
+	}
+
+	try {
+		const decoded = jwt.verify(token, secret);
+		if (!decoded) {
+			res.status(401).json({
+				message: 'authentication failed'
+			});
+		}
+		req.decoded = decoded;
+		next();
+	} catch (error) {
+		res.status(400).json({
+			message: 'invalid token'
+		});
+	}
+};
+
+export default verifyUser;
--- server/queries/insert.js
+++ server/queries/insert.js
@@ -1,6 +1,4 @@
 const query = {
   userSignup: 'INSERT INTO users (firstname,lastname, username, email, password) VALUES ($1, $2, $3, $4,$5) returning *',
-
 };
-
 export default query;
