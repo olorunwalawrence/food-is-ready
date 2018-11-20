12366cfeedabdb2ea1764758937269072178fd70
olorunwalawrence <olorunwalawrence5@gmail.com>
2018-11-20 01:01:52 +0100

    chore(setup) passport-jwt - install passport - install passport-jwt #161979269

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,33 +1,33 @@
 import jwt from 'jsonwebtoken';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 
-consfig();
+config();
 
 const secret = process.env.SECRET;
 
 const verifyUser = (req, res, next) => {
-	const token = req.headers['x-access-token'];
-	if (!token) {
-		res.status(301).json({
-			success: false,
-			message: 'authentication failed, please login'
-		});
-	}
+  const token = req.headers['x-access-token'];
+  if (!token) {
+    res.status(301).json({
+      success: false,
+      message: 'authentication failed, please login'
+    });
+  }
 
-	try {
-		const decoded = jwt.verify(token, secret);
-		if (!decoded) {
-			res.status(401).json({
-				message: 'authentication failed'
-			});
-		}
-		req.decoded = decoded;
-		next();
-	} catch (error) {
-		res.status(400).json({
-			message: 'invalid token'
-		});
-	}
+  try {
+    const decoded = jwt.verify(token, secret);
+    if (!decoded) {
+      res.status(401).json({
+        message: 'authentication failed'
+      });
+    }
+    req.decoded = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({
+      message: 'invalid token'
+    });
+  }
 };
 
 export default verifyUser;
